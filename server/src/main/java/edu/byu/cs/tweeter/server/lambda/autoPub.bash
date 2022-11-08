49787509538e9433f7054e0b460213788856de14
Jacob Zinn <jacobpzinn@gmail.com>
2022-11-07 17:23:24 -0700

    added auto publish script

---
+++ server/src/main/java/edu/byu/cs/tweeter/server/lambda/autoPub.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+arr=(
+        "getFollowing"
+        "getFollowers"
+        "login"
+        "getFollowingCount"
+        "getFollowerCount"
+        "getFeed"
+        "getStory"
+        "getFollow"
+        "getUnfollow"
+        "getIsFollower"
+        "postStatus"
+        "logout"
+        "register"
+        "getUser"
+    )
+for FUNCTION_NAME in "${arr[@]}"
+do
+  aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb:///Users/airbourne25/AndroidStudioProjects/tweeter-milestone-3/server/build/libs/server-all.jar --region us-west-2 &
+done
